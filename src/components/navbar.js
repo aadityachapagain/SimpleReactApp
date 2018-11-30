e6b1a1bf98268a0beecd57e10396c818bda44578
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-11-30 12:36:21 +0545

    playing with Higher order component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {Link , NavLink, withRouter} from 'react-router-dom'
 
 const Navbar = (props) => {
-    setTimeout(() =>{
-        props.history.push('/about')
-    },4000)
+    // setTimeout(() =>{
+    //     props.history.push('/about')
+    // },4000)
     return(
         <nav className="nav-wrappper red darken-3">
             <div className="container">
