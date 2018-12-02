7b33260cd85f58dd24f519a7860e7159009d883f
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-02 18:58:32 +0545

    link to posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar'
-import {BrowserRouter , Route } from 'react-router-dom'
+import {BrowserRouter , Route , Switch } from 'react-router-dom'
 import TodoApp from './components/todoapp'
 import Home from './components/home';
 import About from './components/about';
 import Contact from './components/contact'
+import Post from './components/posts'
 
 
 class App extends Component {
@@ -27,10 +28,13 @@ class App extends Component {
       <BrowserRouter>
         <div className="todo-app">
           <Navbar />
-          <Route exact path='/' component= {Home} />
-          <Route path='/todo' render= {() => <TodoApp todos = {this.state.todos} changeState={this.changeState} />} />
-          <Route path='/about' component= {About}/>
-          <Route path='/contact' component= {Contact}/>
+          <Switch>
+            <Route exact path='/' component= {Home} />
+            <Route path='/todo' render= {() => <TodoApp todos = {this.state.todos} changeState={this.changeState} />} />
+            <Route path='/about' component= {About}/>
+            <Route path='/contact' component= {Contact}/>
+            <Route path="/:user_id" component= {Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
