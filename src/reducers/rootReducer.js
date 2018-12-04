706c054c88f8bee6113dc5aa002e146db9796a76
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-04 16:09:25 +0545

    fixing suxxxxxx

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ const initState = {
     posts:[
         {id: '1', title: 'let laid be laid on look of lust', body: 'Its always nice to have amazing wordplay by the side when you feel bored. It help you to fresh your mood jumpstart your brain and throttle any remaining bad mood swings'},
         {id: '2', title: 'Your argument is sound, nothing but sound', body: 'Here you go One of the epic word play by Benjamin Franklin my old friend was so afraid to lose world in the hand of illuminati which later findings prove that actually true :D'},
-        {id: '1', title: 'Could you on this faire Mountaine leave to feed, And batten on this Moore?', body: 'Nevertheless our old queen Elizabeth II was always greate word player which was often hidden by her beauty of choosing word to strike the opponent right above their head and she was living peagent of that time with such great beauty and intelligence she will always be remembred through her execellence of English which some fellow British Man forget to count !'}
+        {id: '3', title: 'Could you on this faire Mountaine leave to feed, And batten on this Moore?', body: 'Nevertheless our old queen Elizabeth II was always greate word player which was often hidden by her beauty of choosing word to strike the opponent right above their head and she was living peagent of that time with such great beauty and intelligence she will always be remembred through her execellence of English which some fellow British Man forget to count !'}
     ]
 }
 
